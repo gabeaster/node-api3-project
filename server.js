6260b15a7c34dd2fac518f3f11192e0b2723face
gabeaster <gab.easter@live.com>
2020-05-06 18:51:39 -0400

    logger is functional
    setup collection for node-api3-project in postman, testLogger get request to localhost:8000 is return h2 and terminal updates with request details

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const server = express();
 server.use(express.json());
 //invoking custom logger
 server.use(logger);
-
+//logger is functional, postman returns h2 and I get update in terminal about request with and path
 server.get("/", (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
