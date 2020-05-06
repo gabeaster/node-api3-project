3e4639dfb30a2405c975a56b366558dee333f341
gabeaster <gab.easter@live.com>
2020-05-06 18:46:35 -0400

    starting
    setup server.use(express.json()), server.use(logger) and built out function logger(). Need to setup postman next.

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
-const express = require('express');
-
+const express = require("express");
 const server = express();
+server.use(express.json());
+//invoking custom logger
+server.use(logger);
 
-server.get('/', (req, res) => {
+server.get("/", (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  const today = new Date().toISOString();
+  console.log(`[${today}] ${req.method} to ${req.url}`);
+  next();
+}
 
 module.exports = server;
